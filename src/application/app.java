33aee675f044dab61433dbf7bf6864e0f17944da
Iyadi Cyril Osita <iyadicy@gmail.com>
2024-12-29 00:23:38 -0500

    call mainframe

--- src/application/app.java
+++ src/application/app.java
@@ -1,17 +1,12 @@
 package application;
 
-import javax.swing.JFrame;
+import gui.MainFrame;
 import javax.swing.SwingUtilities;
 
-public class app {
-
+public class App {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
-			JFrame mainframe = new JFrame("Swing Low");
-			mainframe.setSize(600, 400);
-			mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			mainframe.setVisible(true);
+			new MainFrame("Swing Low");
 		});
 	}
-
 }
