af87c3e405c65f6de6d9f7def00a0cdc6d6253b8
Iyadi Cyril Osita <iyadicy@gmail.com>
2024-12-29 00:12:11 -0500

    mainframe class

---
+++ src/application/mainframe.java
@@ -0,0 +1,12 @@
+package application;
+
+import javax.swing.JFrame;
+
+public class mainframe extends JFrame {
+
+	public mainframe(String name){
+		super(name);
+	}
+
+	private static final long serialVersionUID = 1L;
+}
