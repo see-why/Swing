a8eecf55e7ade7a6957c1799de2081c4c2521d22
Iyadi Cyril Osita <iyadicy@gmail.com>
2024-12-29 00:12:01 -0500

    add swing app name

--- src/application/app.java
+++ src/application/app.java
@@ -7,7 +7,7 @@ public class app {
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
-			JFrame mainframe = new JFrame();
+			JFrame mainframe = new JFrame("Swing Low");
 			mainframe.setSize(600, 400);
 			mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			mainframe.setVisible(true);
