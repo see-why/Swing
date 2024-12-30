d7f94b654e63ae5bdea6dc53613c40368c09e1d6
Iyadi Cyril Osita <iyadicy@gmail.com>
2024-12-29 22:57:42 -0500

    main panel class

---
+++ src/gui/MainPanel.java
@@ -0,0 +1,14 @@
+package gui;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel {
+	public MainPanel() {
+		super();
+		setBackground(Color.green );
+	}
+
+	private static final long serialVersionUID = 1L;
+}
