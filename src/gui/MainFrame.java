2ca1be537f8f655439a54c1aa56318a65d404263
Iyadi Cyril Osita <iyadicy@gmail.com>
2024-12-29 22:57:07 -0500

    add elements on frame

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,10 +1,16 @@
 package gui;
 
+import java.awt.BorderLayout;
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
 	public MainFrame(String name){
 		super(name);
+
+		setLayout(new BorderLayout());
+		add(new ToolBar(), BorderLayout.NORTH);
+		add(new MainPanel(), BorderLayout.CENTER);
+
 		setSize(600, 400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
