d5c1a832c9300a277d51bd7c98f034bcd0ede0d8
Iyadi Cyril Osita <iyadicy@gmail.com>
2024-12-31 23:33:09 -0500

    toolbar and button expects a colorChanging interface implementation

--- src/gui/ToolBar.java
+++ src/gui/ToolBar.java
@@ -1,12 +1,40 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JToolBar;
 
+import gui.interfaces.ColorChangeListener;
+
+class ColorButtonListener implements ActionListener {
+	private final ColorChangeListener colorChanger;
+	private final Color color;
+	
+	public ColorButtonListener(ColorChangeListener colorChanger, Color color) {
+		this.colorChanger = colorChanger;
+		this.color = color;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		colorChanger.changeColor(color);
+	}
+}
+
+
 public class ToolBar extends JToolBar {
-	public ToolBar() {
-		add(new JButton("Click"));
-		add(new JButton("Submit"));
+	public ToolBar(ColorChangeListener colorChanger) {
+		var redButton = new JButton("Red");
+		var blueButton = new JButton("Blue");
+		
+		redButton.addActionListener(new ColorButtonListener(colorChanger, Color.RED));
+		blueButton.addActionListener(new ColorButtonListener(colorChanger, Color.blue));
+		
+		add(redButton);
+		add(blueButton);
 	}
 
 	private static final long serialVersionUID = 1L;
