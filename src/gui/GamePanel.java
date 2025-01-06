6499da043b18a70498447dcf9d1f98d79ce326b3
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-01-05 21:20:51 -0500

    restore mouse click handler

--- src/gui/GamePanel.java
+++ src/gui/GamePanel.java
@@ -3,19 +3,39 @@ package gui;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
+import models.World;
 
 public class GamePanel extends JPanel {
 	private final static long serialVersionUID = 1L;
-	private final int CELLSIZE = 50;
+
+	private final int CELLSIZE = 100;
 	private int leftRightMargin;
 	private int topBottomMargin;
-	private final static Color BACKGROUND_COLOR = Color.BLACK;
-	private final static Color FOREGROUND_COLOR = Color.GREEN;
-	private final static Color GRID_COLOR = Color.GRAY;
+
+	private final static Color backgroundColor = Color.BLACK;
+	private final static Color foregroundColor = Color.GREEN;
+	private final static Color gridColor = Color.GRAY;
+	
+	private World world;
 	
 	public GamePanel() {
-		setBackground(Color.RED);
+		addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				int row = (e.getY() - topBottomMargin) / CELLSIZE;
+				int column = (e.getX() - leftRightMargin) / CELLSIZE;
+				
+				if (row >= world.getRows() || column >= world.getColumns()) {
+					return;
+				}
+				
+				world.setCellState(row, column, !world.getCellState(row, column));
+				repaint();
+			}
+			
+		});
 	}
 	
 	@Override
@@ -27,16 +47,36 @@ public class GamePanel extends JPanel {
 		
 		leftRightMargin = ((width % CELLSIZE) + CELLSIZE) / 2;
 		topBottomMargin = ((height % CELLSIZE) + CELLSIZE) / 2;
+		
+		int rows = width -  2 * leftRightMargin / CELLSIZE;
+		int columns = height - 2 * topBottomMargin / CELLSIZE;
+		
+		if (world == null) {
+			world = new World(rows, columns);
+		}
+		
+		world.setCellState(0, 0, true);
+		world.setCellState(5, 5, true);
 
-		g2.setColor(BACKGROUND_COLOR);
+		g2.setColor(backgroundColor);
 		g2.fillRect(0, 0, width, height);
 		
-		drawGrid(g2, width, height);
 		fillCell(g2, 3, 5, true);
+		fillGrid(g2, rows, columns);
+		drawGrid(g2, width, height);
+	}
+	
+	private void fillGrid(Graphics g2, int rows, int columns) {
+		for (int x = 0; x < rows; x++) {
+			for (int y = 0; y < columns; y++) {
+				fillCell(g2, x, y, world.getCellState(x, y));
+			} 
+		}
+		
 	}
 	
 	private void fillCell(Graphics g2, int row, int col, boolean status) {
-		Color color = status ? FOREGROUND_COLOR : BACKGROUND_COLOR;
+		Color color = status ? foregroundColor : backgroundColor;
 		g2.setColor(color);
 		
 		int x = leftRightMargin + col * CELLSIZE;
@@ -47,7 +87,7 @@ public class GamePanel extends JPanel {
 	}
 	
 	private void drawGrid(Graphics g, int width, int height) {
-		g.setColor(GRID_COLOR);
+		g.setColor(gridColor);
 		
 		for (int x = leftRightMargin; x <= width - leftRightMargin; x += CELLSIZE) {
 			g.drawLine(x, topBottomMargin, x, height - topBottomMargin);
