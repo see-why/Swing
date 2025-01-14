0fc54f2d7054eebc5185b02d3ed74d34eeff63ce
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-01-13 22:59:10 -0500

    check if file exists

--- src/test/CheckLogic.java
+++ src/test/CheckLogic.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 import models.World;
 
 public class CheckLogic {
@@ -12,20 +11,27 @@ public class CheckLogic {
 		World world = new World(4, 7);
 		world.countNeighbours(0, 0);
 		world.countNeighbours(4, 7);
+		System.out.println();
 		
 		File currentDirectory = new File(".");
 		System.out.println(currentDirectory.getAbsolutePath());
 		System.out.println(currentDirectory.getCanonicalPath());
+		System.out.println();
 		
 		for(String f: currentDirectory.list()) {
 			System.out.println(f);
 		}
-		
+		System.out.println();
+
 		String text = "It's\nbeen\na\nwhile.";
 		Path path = Paths.get("text.txt");
 		Files.write(path, text.getBytes());
 		
 		String readText = Files.readString(path);
 		System.out.println(readText);
+		System.out.println();
+
+		String fileLocation = "/Users/ocyril/Documents/eclipse-workspace/Swing/text.txt";
+		System.out.println(new File(fileLocation).exists());
 	}
 }
