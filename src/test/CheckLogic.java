f4da807c1fe0505b7ca8efbb9b0e9ac18c17016d
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-01-13 22:37:48 -0500

    learning about File class

--- src/test/CheckLogic.java
+++ src/test/CheckLogic.java
@@ -1,10 +1,21 @@
 package test;
+import java.io.File;
+import java.io.IOException;
+
 import models.World;
 
 public class CheckLogic {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		World world = new World(4, 7);
 		world.countNeighbours(0, 0);
 		world.countNeighbours(4, 7);
+		
+		File currentDirectory = new File(".");
+		System.out.println(currentDirectory.getAbsolutePath());
+		System.out.println(currentDirectory.getCanonicalPath());
+		
+		for(String f: currentDirectory.list()) {
+			System.out.println(f);
+		}
 	}
 }
