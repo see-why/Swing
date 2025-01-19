8f8fb07cc8262ac1e60602b2fe785cfabc0312fe
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-01-18 23:59:59 -0500

    add menu bars

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,11 +1,15 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
 	private static final GamePanel gamePanel = new GamePanel();
 
 	public MainFrame(String name){
@@ -13,6 +17,18 @@ public class MainFrame extends JFrame {
 
 		setLayout(new BorderLayout());
 		add(gamePanel, BorderLayout.CENTER);
+		
+		MenuItem openItem = new MenuItem("Open");
+		MenuItem saveItem = new MenuItem("Save");
+		
+		Menu fileMenu = new Menu("File");
+		fileMenu.add(openItem);
+		fileMenu.add(saveItem);
+		
+		MenuBar menuBar = new MenuBar();
+		menuBar.add(fileMenu);
+		
+		setMenuBar(menuBar);
 		
 		addKeyListener(new KeyAdapter() {
 			@Override
@@ -38,6 +54,4 @@ public class MainFrame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
-
-	private static final long serialVersionUID = 1L;
 }
