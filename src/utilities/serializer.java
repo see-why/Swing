ed0ec35d119f1e98ad374d2f9d216f90babe1105
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-01-19 02:03:19 -0500

    display error to user

--- src/utilities/serializer.java
+++ src/utilities/serializer.java
@@ -6,33 +6,34 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import javax.swing.JOptionPane;
 
 public class serializer {
-	public static Object deserializeObject(String pathString) {
-		Object result = null;
-		try(var os = new ObjectInputStream(new FileInputStream(pathString))) {
-			result = os.readObject();
-			System.out.println(result);
-		} catch (FileNotFoundException e) {
-			System.err.printf("File not found: %s \n", pathString);
-		} catch (IOException e) {
-			System.err.printf("Error reading file: %s \n", pathString);
-		} catch (ClassNotFoundException e) {
-			System.err.printf("Cannot read object from file: %s \n", pathString);
-		}
-		System.out.println("deserializeObject Completed! \n");
-		return result;
-	}
-	
-	public static void serializeObject(Object obj, String pathString) {		
-		try(var os = new ObjectOutputStream(new FileOutputStream(pathString))) {
-			os.writeObject(obj);
-		} catch (FileNotFoundException e) {
-			System.err.printf("File not found: %s \n", pathString);
-		} catch (IOException e) {
-			System.err.printf("Error wrting file: %s \n", pathString);
-		}
-		System.out.println("serializeObject Completed! \n");
-	}
+    private static void showError(String message) {
+        JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
+    }
 
+    public static Object deserializeObject(String pathString) {
+        Object result = null;
+        try(var os = new ObjectInputStream(new FileInputStream(pathString))) {
+            result = os.readObject();
+        } catch (FileNotFoundException e) {
+            showError("File not found: " + pathString);
+        } catch (IOException e) {
+            showError("Error reading file: " + pathString + "\n" + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            showError("Cannot read object from file: " + pathString + "\nInvalid file format");
+        }
+        return result;
+    }
+    
+    public static void serializeObject(Object obj, String pathString) {        
+        try(var os = new ObjectOutputStream(new FileOutputStream(pathString))) {
+            os.writeObject(obj);
+        } catch (FileNotFoundException e) {
+            showError("File not found: " + pathString);
+        } catch (IOException e) {
+            showError("Error writing file: " + pathString + "\n" + e.getMessage());
+        }
+    }
 }
